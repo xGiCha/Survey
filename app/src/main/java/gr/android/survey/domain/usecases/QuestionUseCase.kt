4729fceca880f4e14dc46ec2fc15af2b2b34ad7f
Chatzigiannis <giannisxatz16@gmail.com>
2024-06-03 16:25:05 +0300

    updates on survey app

--- app/src/main/java/gr/android/survey/domain/usecases/QuestionUseCase.kt
+++ app/src/main/java/gr/android/survey/domain/usecases/QuestionUseCase.kt
@@ -12,9 +12,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
--- app/src/main/java/gr/android/survey/ui/composable/modals/DebouncedButton.kt
+++ app/src/main/java/gr/android/survey/ui/composable/modals/DebouncedButton.kt
@@ -1,18 +1,11 @@
 package gr.android.survey.ui.composable.modals
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.unit.dp
-import gr.android.survey.R
 import gr.android.survey.utils.debounce
 
 @Composable
